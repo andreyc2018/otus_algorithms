bfd493f770ce72e62d6764c0f99e033055cea491
Andrey Chichinov <sysdos286@yahoo.com>
2019-01-10 04:49:34 -0500

    iarray wip

--- lesson_02/IArray.h
+++ lesson_02/IArray.h
@@ -1,50 +1,67 @@
 #pragma once
 #include "BArray.h"
+#include <tuple>
 
 template<typename T>
 class IArray
 {
   private:
+    using block_t = BArray<T>;
+    using array_t = BArray<block_t*>;
+
     int _size;
-    BArray<BArray<T>>* arr_;
+    array_t* arr_;
     int block_size_;
 
     void relocate()
     {
-        arr_->add(0, BArray<T>(block_size_));
+        block_t* block = new block_t(block_size_);
+        arr_->add(_size, block);
+        _size++;
     }
 
   public:
-    explicit IArray(int block_size = DefaultBlock) : block_size_(block_size)
-    {
-        arr_ = new BArray<BArray<T>>(block_size_);
-        arr_->add(0, BArray<T>(block_size));
-    }
+    explicit IArray(int block_size = DefaultBlock)
+      : _size(0), arr_(new array_t(block_size)), block_size_(block_size)
+    {}
 
-    ~IArray()
-    {
-        if (arr_ != nullptr)
-            delete [] arr_;
-    }
+    ~IArray() { delete arr_; }
 
     void add(int index, T element)
     {
-        if (size() <= index) {
+        int index1;
+        int index2;
+        std::tie(index1, index2) = get_index(index);
+
+        if (index1 >= arr_->size()) {
             relocate();
         }
-        int index1 = index / block_size_;
-        int index2 = index % block_size_;
-        arr_->get(index1).add(index2, element);
+        arr_->get(index1)->add(index2, element);
     }
 
-    void set(int index, T element) { arr_[index] = element; }
+    void set(int index, T element)
+    {
+        int index1;
+        int index2;
+        std::tie(index1, index2) = get_index(index);
+        arr_->get(index1)->set(index2, element);
+    }
 
-    int size() { return arr_->size() * block_size_; }
+    int size() { return _size; }
 
     T get(int index)
     {
+        int index1;
+        int index2;
+        std::tie(index1, index2) = get_index(index);
+        return arr_->get(index1)->get(index2);
+    }
+
+  private:
+    std::tuple<int, int> get_index(int index)
+    {
         int index1 = index / block_size_;
         int index2 = index % block_size_;
-        return arr_->get(index1).get(index2);
+        return std::make_tuple(index1, index2);
     }
 };
--- lesson_02/test_iarray.cpp
+++ lesson_02/test_iarray.cpp
@@ -4,7 +4,20 @@
 
 TEST(BArray, Init)
 {
-    IArray<int> b;
+    IArray<int> b(3);
 
     EXPECT_EQ(0, b.size());
+}
+
+TEST(BArray, Add)
+{
+    IArray<int> b(2);
+
+    EXPECT_EQ(0, b.size());
+
+    for (int i = 0; i<10; i++)
+        b.add(i, i*i);
+
+    for (int i = 0; i < 5; i++)
+        std::cout << b.get(i) << "\n";
 }
