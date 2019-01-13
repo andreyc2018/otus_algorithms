fd45aac2ac146c804faffd067705e1add93b1d5f
Andrey Chichinov <sysdos286@yahoo.com>
2019-01-13 01:14:56 -0500

    use uint2048_t to test power

--- lesson_03/power_compare.cpp
+++ lesson_03/power_compare.cpp
@@ -1,7 +1,11 @@
 #include "power.h"
 #include <stopwatch.h>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
 #include <cmath>
+
+using namespace boost::multiprecision;
+using uint2048_t = number<cpp_int_backend<2048, 2048, unsigned_magnitude, unchecked, void>>;
 
 auto mul = [](auto a, auto b) { return a * b; };
 
@@ -9,7 +13,7 @@ template<typename I>
 I power_1(I a, I b)
 {
     I r = 1;
-    for (int i = 0; i < b; ++i) {
+    for (I i = 0; i < b; ++i) {
         r *= a;
     }
     return r;
@@ -26,17 +30,21 @@ int main(int, char**)
     stop_watch_t t;
 
     t.start();
-    auto r = std::pow(12, 8);
+    uint64_t a = 12;
+    uint64_t b = 27;
+    auto r = std::pow(a, b);
     auto elapsed = t.stop();
-    std::cout << "std::pow = " << static_cast<uint64_t>(r) << " : " << elapsed << " " << t.period() << "\n";
+    std::cout << "std::pow = " << static_cast<uint2048_t>(r) << " : " << elapsed << " " << t.period() << "\n";
 
+    uint2048_t x = 12;
+    uint2048_t y = 27;
     t.start();
-    auto ra = power_1(12, 8); // 429981696
+    auto ra = power_1(x, y); // 429981696
     elapsed = t.stop();
     std::cout << "power_1  = " << ra << " : " << elapsed << " " << t.period() << "\n";
 
     t.start();
-    ra = power_2(12, 8);
+    ra = power_2(x, y);
     elapsed = t.stop();
     std::cout << "power_2  = " << ra << " : " << elapsed << " " << t.period() << "\n";
     return 0;
