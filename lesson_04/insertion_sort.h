daa927d8b5c7b2d6583c06f315c5175d9690cce0
Andrey Chichinov <sysdos286@yahoo.com>
2019-01-25 22:28:23 -0500

    insertin sort

--- lesson_04/insertion_sort.h
+++ lesson_04/insertion_sort.h
@@ -1,1 +1,18 @@
 #pragma once
+
+template<typename C>
+void insertion_sort(C& array)
+{
+    using value_type = typename C::value_type;
+    using size_type = typename C::size_type;
+    
+    for (size_type next_idx = 1; next_idx < array.size(); ++next_idx) {
+        value_type insert_value = array[next_idx];
+        auto insert_idx = next_idx;
+        while(insert_idx > 0 && array[insert_idx-1] > insert_value){
+            array[insert_idx] = array[insert_idx-1];
+            --insert_idx;
+        }
+        array[insert_idx] = insert_value;
+    }
+}
--- lesson_04/test_insertion_sort.cpp
+++ lesson_04/test_insertion_sort.cpp
@@ -1,7 +1,25 @@
+#include "insertion_sort.h"
+#include <random>
+#include <algorithm>
+#include <iterator>
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main(int argc, char *argv[])
+TEST(InsertionSort, Unsorted)
 {
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // std::vector<int> v = {1, 2, 3, 4 };
 
-    return 0;
+    std::random_device rd;
+    std::mt19937 g(rd());
+ 
+    std::shuffle(v.begin(), v.end(), g);
+ 
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n";
+
+    insertion_sort(v);
+
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << "\n";
 }
