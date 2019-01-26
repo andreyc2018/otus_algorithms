99eefb6fc6e2f9216315424f04b5fe11a78cc7bb
Andrey Chichinov <sysdos286@yahoo.com>
2019-01-26 00:05:25 -0500

    add shell_sort

--- lesson_04/insertion_sort.h
+++ lesson_04/insertion_sort.h
@@ -1,21 +1,32 @@
 #pragma once
 
+template<typename C, typename size_type, typename value_type>
+void find_and_insert(C& array, size_type insert_idx, value_type value, size_type offset)
+{
+    while(insert_idx > (offset - 1) && array[insert_idx-offset] > value){
+        array[insert_idx] = array[insert_idx-offset];
+        insert_idx -= offset;
+    }
+
+    array[insert_idx] = value;
+}
+
 template<typename C>
 void insertion_sort(C& array)
 {
-    using value_type = typename C::value_type;
     using size_type = typename C::size_type;
     
     for (size_type next_idx = 1; next_idx < array.size(); ++next_idx) {
+        find_and_insert(array, next_idx, array[next_idx], static_cast<size_type>(1));
+    }
+}
 
-        value_type insert_value = array[next_idx];
-        auto insert_idx = next_idx;
-
-        while(insert_idx > 0 && array[insert_idx-1] > insert_value){
-            array[insert_idx] = array[insert_idx-1];
-            --insert_idx;
+template<typename C, typename size_type>
+void shell_sort(C& array, size_type interval)
+{
+    for (; interval > 0; interval = (interval - 1)  / 3) {
+        for (auto next_idx = interval; next_idx < array.size(); ++next_idx) {
+            find_and_insert(array, next_idx, array[next_idx], interval);
         }
-
-        array[insert_idx] = insert_value;
     }
 }
--- lesson_04/test_insertion_sort.cpp
+++ lesson_04/test_insertion_sort.cpp
@@ -5,20 +5,21 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+namespace {
 std::random_device rd;
 std::mt19937 rng(rd());
+}
+
+constexpr size_t InsertArraySize = 10000;
+constexpr size_t ArraySize = 50000;
 
 TEST(InsertionSort, Unsorted)
 {
     std::vector<int> array;
-    std::generate_n(std::back_inserter(array), 100, std::ref(rng)); 
+    std::generate_n(std::back_inserter(array), InsertArraySize, std::ref(rng));
+
     std::vector<int> expected_array = array;
     std::sort(expected_array.begin(), expected_array.end());
-    // std::copy(array.begin(), array.end(), std::ostream_iterator<int>(std::cout, " "));
-    // std::cout << "\n";
-    
-    // std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-    // std::cout << "\n";
 
     insertion_sort(array);
 
@@ -27,8 +28,145 @@ TEST(InsertionSort, Unsorted)
                         expected_array.begin(), expected_array.end(), 
                         std::inserter(diff, diff.begin()));
     EXPECT_EQ(0, diff.size());
+}
 
+TEST(InsertionSort, Sorted)
+{
+    std::vector<int> array(InsertArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
 
-    // std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+    insertion_sort(array);
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(), 
+                        expected_array.begin(), expected_array.end(), 
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(InsertionSort, PartiallySorted)
+{
+    std::vector<int> array(InsertArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
+
+    // std::copy(array.begin(), array.end(), std::ostream_iterator<int>(std::cout, " "));
     // std::cout << "\n";
+
+    auto from = std::next(std::begin(array), 1000);
+    auto to = std::next(from, 200);
+    // std::cout << "from = " << *from << "\n";
+    // std::cout << "to   = " << *to << "\n";
+    std::shuffle(from, to, rng);
+
+    // std::copy(array.begin(), array.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << "\n";
+    
+    insertion_sort(array);
+
+    // std::copy(array.begin(), array.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << "\n";
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(), 
+                        expected_array.begin(), expected_array.end(), 
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(InsertionSort, Reversed)
+{
+    std::vector<int> array(InsertArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
+    
+    std::reverse(array.begin(), array.end());
+
+    insertion_sort(array);
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(), 
+                        expected_array.begin(), expected_array.end(), 
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+size_t knuth_interval(size_t size)
+{
+    size_t interval = 1;
+
+    /* calculate interval*/
+    while (interval < size/3) {
+        interval = interval * 3 + 1;
+    }
+    return interval;
+}
+
+TEST(ShellSort, Unsorted)
+{
+    std::vector<int> array;
+    std::generate_n(std::back_inserter(array), ArraySize, std::ref(rng));
+
+    std::vector<int> expected_array = array;
+    std::sort(expected_array.begin(), expected_array.end());
+
+    shell_sort(array, knuth_interval(array.size()));
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(),
+                        expected_array.begin(), expected_array.end(),
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(ShellSort, Sorted)
+{
+    std::vector<int> array(ArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
+
+    shell_sort(array, knuth_interval(array.size()));
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(),
+                        expected_array.begin(), expected_array.end(),
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(ShellSort, PartiallySorted)
+{
+    std::vector<int> array(ArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
+
+    auto from = std::next(std::begin(array), 1000);
+    auto to = std::next(from, 200);
+    std::shuffle(from, to, rng);
+
+    shell_sort(array, knuth_interval(array.size()));
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(),
+                        expected_array.begin(), expected_array.end(),
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(ShellSort, Reversed)
+{
+    std::vector<int> array(ArraySize);
+    std::iota(array.begin(), array.end(), 100000);
+    std::vector<int> expected_array = array;
+
+    std::reverse(array.begin(), array.end());
+
+    shell_sort(array, knuth_interval(array.size()));
+
+    std::vector<int> diff;
+    std::set_difference(array.begin(), array.end(),
+                        expected_array.begin(), expected_array.end(),
+                        std::inserter(diff, diff.begin()));
+    EXPECT_EQ(0, diff.size());
 }
