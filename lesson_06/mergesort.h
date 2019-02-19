55d708aeeb40e0b9db88e8d21ca1bb02e4a23e99
Anery Chichinov <sysdos286@yahoo.com>
2019-02-18 23:05:04 -0500

    wip #6

--- lesson_06/mergesort.h
+++ lesson_06/mergesort.h
@@ -1,1 +1,69 @@
 #pragma once
+
+#include <iterator>
+
+namespace otus {
+
+template <typename T>
+class view_range
+{
+    public:
+        using iterator = typename T::iterator;
+        using difference_type = typename T::difference_type;
+        using size_type = typename T::size_type;
+
+        view_range(T& array, iterator b, iterator e)
+            : array_(array), begin_(b), end_(e) {}
+
+        iterator begin() const { return begin_; }
+        iterator end() const { return end_; }
+        iterator offset(difference_type offset) const { return std::next(begin_, offset); }
+
+        size_type size() const { return std::distance(begin_, end_); }
+
+        T& base() { return array_; };
+
+    private:
+        T& array_;
+        iterator begin_;
+        iterator end_;
+};
+
+template <typename T>
+view_range<T> merge(view_range<T>& left, view_range<T>& right)
+{
+    view_range<T> result(left.base(), left.begin(), right.end());
+    return result;
+}
+
+template <typename T>
+view_range<T> split_merge(view_range<T>& view)
+{
+    if (view.size() < 2) {
+        return view;
+    }
+
+    auto m = view.size() / 2;
+    view_range<T> left(view.base(), view.begin(), view.offset(m));
+    split_merge(left);
+
+    view_range<T> right(view.base(), view.offset(m), view.end());
+    split_merge(right);
+
+    auto result = merge(left, right);
+
+    return result;
+}
+
+template <typename T>
+void merge_sort(T& array)
+{
+    if (array.size() < 2) {
+        return;
+    }
+    view_range<T> view(array, std::begin(array), std::end(array));
+
+    split_merge(view);
+}
+
+}
--- lesson_06/test_mergesort.cpp
+++ lesson_06/test_mergesort.cpp
@@ -1,6 +1,17 @@
 #include "mergesort.h"
 
-#include <stopwatch.h>
-#include <random>
-#include <algorithm>
+#include <test_tools.h>
 #include <gtest/gtest.h>
+
+TEST(MergeSort, Unsorted)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_random_array(array, expected_array, 9/*test_tools::ArraySize*/);
+
+    test_tools::timed_run(std::cout, [&array]() { otus::merge_sort(array); }, "Unsorted:        ");
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    EXPECT_EQ(0, diff.size());
+}
