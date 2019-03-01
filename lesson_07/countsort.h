ae88ffcb66f175488a33fc1bcdfb1324addfb05d
Andrey Chichinov <sysdos286@yahoo.com>
2019-03-01 08:16:36 -0500

    added struct of vectors

--- lesson_07/countsort.h
+++ lesson_07/countsort.h
@@ -4,46 +4,116 @@
 #include <iostream>
 
 namespace otus {
-
-struct CounterValue
-{
-    size_t value = 0;
-    size_t counter = 0;
-};
-
+namespace vos {
+/**
+ * @brief count_sort_vos - Count sort using vector of structs
+ * @param array - array to sort
+ * @param k - max element in te array
+ */
 template <typename T>
 void count_sort(T& array, typename T::value_type k)
 {
+    using element_t = decltype (k);
+    struct CounterValue
+    {
+        CounterValue(element_t v, element_t c) : value(v), count(c) {}
+        element_t value = 0;
+        element_t count = 0;
+    };
+
     using counter_t = std::vector<CounterValue>;
     counter_t counter;
     counter.reserve(k + 1);
-    for (decltype (k) i = 0; i <= k; ++i) {
-        counter.emplace_back();
+    for (element_t i = 0; i <= k; ++i) {
+        counter.emplace_back(i, 0);
     }
 
-    for (const auto& i : counter) {
-        std::cout << "counter[" << i.first << "] = " << i.second << "\n";
-    }
+//    for (const auto& i : counter) {
+//        std::cout << "value = " << i.value << ", count = " << i.count << "\n";
+//    }
 
     for (const auto& i : array) {
-        ++counter[i];
+        counter[i].count ++;
+//        std::cout << "counter[" << i << "] = " << counter[i].count << "\n";
     }
 
+    int j = 0;
     for (const auto& i : counter) {
-        std::cout << "counter[" << i.first << "] = " << i.second << "\n";
+        std::cout << "j = " << j << " value = " << i.value << ", count = " << i.count << "\n";
+        ++j;
     }
 
-    size_t counter_pointer = 0;
-    size_t array_pointer = 0;
+    element_t counter_pointer = 0;
+    element_t array_pointer = 0;
 
-    while (counter_pointer < k) {
-        if (counter[counter_pointer] == 0) {
+    while (counter_pointer <= k) {
+        if (counter[counter_pointer].count == 0) {
             counter_pointer += 1;
         } else {
-            counter[counter_pointer] -= 1;
-            array[array_pointer];
+            counter[counter_pointer].count -= 1;
+            array[array_pointer] = counter[counter_pointer].value;
+            array_pointer ++;
         }
     }
+}
+
+}
+
+namespace sov {
+/**
+ * @brief count_sort_vos - Count sort using struct of vectors
+ * @param array - array to sort
+ * @param k - max element in te array
+ */
+template <typename T>
+void count_sort(T& array, typename T::value_type k)
+{
+    using element_t = decltype (k);
+    struct CounterValue
+    {
+        CounterValue() = default;
+        CounterValue(element_t k) { value.reserve(k), count.reserve(k); }
+        void push_back(element_t v, element_t c) {
+            value.push_back(v);
+            count.push_back(c);
+        }
+
+        element_t& operator[](element_t& i) {
+            return count[i];
+        }
+
+        std::vector<element_t> value;
+        std::vector<element_t> count;
+    };
+
+    using counter_t = CounterValue;
+    counter_t counter;
+    for (element_t i = 0; i <= k; ++i) {
+        counter.push_back(i, 0);
+    }
+
+    for (auto& i : array) {
+        counter[i] ++;
+//        std::cout << i << " : " << counter.value[i] << " : " << counter.count[i] << "\n";
+    }
+
+    for (element_t i = 0; i <= k; ++i) {
+        std::cout << "i = " << i << " value = " << counter.value[i] << ", count = " << counter.count[i] << "\n";
+    }
+
+    element_t counter_pointer = 0;
+    element_t array_pointer = 0;
+
+    while (counter_pointer <= k) {
+        if (counter.count[counter_pointer] == 0) {
+            counter_pointer += 1;
+        } else {
+            counter.count[counter_pointer] -= 1;
+            array[array_pointer] = counter.value[counter_pointer];
+            array_pointer ++;
+        }
+    }
+}
 }
 
 }
--- lesson_07/test_countsort.cpp
+++ lesson_07/test_countsort.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <algorithm>
 
-TEST(MergeSort, Unsorted_Short)
+TEST(MergeSort, Unsorted_Short_Vos)
 {
     std::vector<int> array;
     std::vector<int> expected_array;
@@ -13,7 +13,25 @@ TEST(MergeSort, Unsorted_Short)
     test_tools::debug_print(std::cout, expected_array);
 
     auto k = expected_array.back();
-    otus::count_sort(array, k);
+    otus::vos::count_sort(array, k);
+    test_tools::debug_print(std::cout, array);
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    EXPECT_EQ(0, diff.size());
+}
+
+TEST(MergeSort, Unsorted_Short_Sov)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_random_array(array, expected_array, 15, 10);
+    test_tools::debug_print(std::cout, array);
+    test_tools::debug_print(std::cout, expected_array);
+
+    auto k = expected_array.back();
+    otus::sov::count_sort(array, k);
+    test_tools::debug_print(std::cout, array);
 
     std::vector<int> diff;
     test_tools::diff_arrays(array, expected_array, diff);
