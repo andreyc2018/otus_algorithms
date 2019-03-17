65fd2c4089b35e68894241be9c794078e30dcb2f
Andrey Chichinov <sysdos286@yahoo.com>
2019-03-17 18:45:32 -0400

    readix sort using original count sort

--- lesson_07/radixsort.h
+++ lesson_07/radixsort.h
@@ -1,4 +1,55 @@
-#ifndef RADIXSORT_H
-#define RADIXSORT_H
+#pragma once
+#include <iostream>
+#include <test_tools.h>
 
-#endif // RADIXSORT_H
+namespace otus {
+
+template <typename T>
+void count_sort(T& array, typename T::value_type k, typename T::value_type idx)
+{
+    using element_t = decltype (k);
+    T count (k, 0);
+
+//    std::cout << "\t";
+//    test_tools::debug_print(std::cout, count);
+
+    for (const auto& item : array) {
+        count[(item/idx)%k] ++;
+    }
+
+//    std::cout << "\t";
+//    test_tools::debug_print(std::cout, count);
+
+    for (element_t i = 1; i <= k; ++i) {
+        count[i] += count[i-1];
+    }
+
+//    std::cout << "\t";
+//    test_tools::debug_print(std::cout, count);
+
+    T sorted(array.size());
+
+    std::for_each(std::rbegin(array), std::rend(array), [&sorted, &count, &k, &idx](const element_t& item) {
+        auto i = (item/idx)%k;
+        count[i] -= 1;
+        sorted[count[i]] = item;
+    });
+
+//    std::cout << "\t";
+//    test_tools::debug_print(std::cout, sorted);
+
+    std::copy(std::begin(sorted), std::end(sorted), std::begin(array));
+}
+
+template <typename T>
+void radix_sort(T& array, typename T::value_type k)
+{
+//    std::cout << "k = " << k << "\n";
+    for (int i = 1; i <= k; i *= 10) {
+//        std::cout << "i = " << i << "\n\t";
+//        test_tools::debug_print(std::cout, array);
+        count_sort(array, 10, i);
+    }
+}
+
+}
---
+++ lesson_07/test_radixsort.cpp
@@ -0,0 +1,87 @@
+#include "radixsort.h"
+#include <test_tools.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <algorithm>
+
+static constexpr size_t ArraySize = 150000;
+
+TEST(RadixSort, PrintDigits)
+{
+    std::vector<int> array { 301, 4987, 23, 1 };
+    auto k = *(std::max_element(std::begin(array), std::end(array)));
+
+    for (int i = 1; i <= k; i *= 10) {
+        std::cout << "i = " << i << "\n";
+        for (const auto& a : array) {
+            std::cout << "\t" << a << " => " << (a/i)%10 << "\n";
+        }
+    }
+    otus::radix_sort(array, k);
+}
+
+TEST(RadixSort, Unsorted_Short)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_random_array(array, expected_array, 5, 1000);
+    test_tools::debug_print(std::cout, array);
+    test_tools::debug_print(std::cout, expected_array);
+
+    auto k = expected_array.back();
+    otus::radix_sort(array, k);
+    test_tools::debug_print(std::cout, array);
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    size_t expected = 0;
+    EXPECT_EQ(expected, diff.size());
+}
+
+TEST(CountSort, Unsorted)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_random_array(array, expected_array, ArraySize, 10000);
+
+    auto k = expected_array.back();
+    test_tools::timed_run(std::cout,
+                          [&array, k]() { otus::radix_sort(array, k); }, "Unsorted:          ");
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    size_t expected = 0;
+    EXPECT_EQ(expected, diff.size());
+}
+
+TEST(CountSort, Sorted)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_sorted_array(array, expected_array, ArraySize, 10000);
+
+    auto k = expected_array.back();
+    test_tools::timed_run(std::cout,
+                          [&array, k]() { otus::radix_sort(array, k); }, "Sorted:            ");
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    size_t expected = 0;
+    EXPECT_EQ(expected, diff.size());
+}
+
+TEST(CountSort, Reversed)
+{
+    std::vector<int> array;
+    std::vector<int> expected_array;
+    test_tools::create_reversed_array(array, expected_array, ArraySize, 10000);
+
+    auto k = expected_array.back();
+    test_tools::timed_run(std::cout,
+                          [&array, k]() { otus::radix_sort(array, k); }, "Reversed:          ");
+
+    std::vector<int> diff;
+    test_tools::diff_arrays(array, expected_array, diff);
+    size_t expected = 0;
+    EXPECT_EQ(expected, diff.size());
+}
