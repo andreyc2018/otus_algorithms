73c1ac2cc4735a377d2c9153e78436c546723d05
Anery Chichinov <sysdos286@yahoo.com>
2019-05-20 21:39:22 -0400

    wip bitboard

---
+++ 03_programming_chess/bitboard/bitboard.h
@@ -0,0 +1,21 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
+#include <stdint.h>
+#include <bitset>
+
+class BitBoard
+{
+public:
+    using bitboard_t = std::bitset<sizeof (uint64_t)>;
+
+    BitBoard() : board_(0) {}
+    BitBoard(uint64_t value) : board_(value) {}
+
+    void shift_left(int steps) { board_.; }
+
+private:
+    bitboard_t board_;
+};
+
+#endif // BITBOARD_H
