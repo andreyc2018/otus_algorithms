3f4495de34afb12713ba7387ab2ccc159c26049f
Andrey Chichinov <sysdos286@yahoo.com>
2019-05-29 22:21:06 -0400

    add fen description

--- 03_programming_chess/fen_parser.h
+++ 03_programming_chess/fen_parser.h
@@ -9,6 +9,35 @@ using board_t      = std::array<std::array<char, 8>, 8>;
 using char_board_t = std::array<char, 64>;
 
 /**
+ * A FEN "record" defines a particular game position, all in one text line and using only the ASCII
+character set. A text file with only FEN data records should have the file extension ".fen".[1]
+
+A FEN record contains six fields. The separator between fields is a space. The fields are:
+
+    Piece placement (from White's perspective). Each rank is described, starting with rank 8 and
+ending with rank 1; within each rank, the contents of each square are described from file "a"
+through file "h". Following the Standard Algebraic Notation (SAN), each piece is identified by a
+single letter taken from the standard English names (pawn = "P", knight = "N", bishop = "B", rook =
+"R", queen = "Q" and king = "K").[1] White pieces are designated using upper-case letters ("PNBRQK")
+while black pieces use lowercase ("pnbrqk"). Empty squares are noted using digits 1 through 8 (the
+number of empty squares), and "/" separates ranks.
+
+    Active color. "w" means White moves next, "b" means Black moves next.
+
+    Castling availability. If neither side can castle, this is "-". Otherwise, this has one or more
+letters: "K" (White can castle kingside), "Q" (White can castle queenside), "k" (Black can castle
+kingside), and/or "q" (Black can castle queenside).
+
+    En passant target square in algebraic notation. If there's no en passant target square, this is
+"-". If a pawn has just made a two-square move, this is the position "behind" the pawn. This is
+recorded regardless of whether there is a pawn in position to make an en passant capture.[2]
+
+    Halfmove clock: This is the number of halfmoves since the last capture or pawn advance. This is
+used to determine if a draw can be claimed under the fifty-move rule.
+
+    Fullmove number: The number of the full move. It starts at 1, and is incremented after Black's
+move.
+
  * rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
  * r...R - pieces and empty spaces
  * w - next turn in white or
