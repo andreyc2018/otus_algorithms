e529e4aec131f6eb4c5af890c702d2f0a574fd07
Anery Chichinov <sysdos286@yahoo.com>
2019-07-22 21:12:32 -0400

    avltree wip

--- 09_avl_decart_tree/avltree.cpp
+++ 09_avl_decart_tree/avltree.cpp
@@ -1,12 +1,12 @@
 #include "avltree.h"
+#include <algorithm>
 
 hw::AvlTree::~AvlTree() { destroyTree(root_); }
 
 void hw::AvlTree::insert(int value)
 {
-    if (root_ == nullptr) {
-        root_ = new hw::AvlNode(value);
-    }
+    auto n = new hw::AvlNode(value);
+    root_  = insertNodeAt(root_, n);
 }
 
 bool hw::AvlTree::empty() const { return (root_ == nullptr || root_->height == -1); }
@@ -15,4 +15,32 @@ int hw::AvlTree::height() const { return (root_ == nullptr) ? -1 : root_->height
 
 int hw::AvlTree::balance() const { return (root_ == nullptr) ? 0 : root_->balance; }
 
+void hw::AvlTree::rebalance(hw::AvlNode* n)
+{
+    if (n->balance >= -1 || n->balance <= 1) {
+        return;
+    }
+}
+
 void hw::AvlTree::destroyTree(hw::AvlNode* n) { delete n; }
+
+hw::AvlNode* hw::AvlTree::insertNodeAt(hw::AvlNode* p, hw::AvlNode* n)
+{
+    if (p == nullptr || n == nullptr) {
+        return n;
+    }
+
+    if (n->value < p->value) {
+        p->left = insertNodeAt(p->left, n);
+    } else {
+        p->right = insertNodeAt(p->right, n);
+    }
+
+    auto l    = (p->left != nullptr) ? p->left->height : 0;
+    auto r    = (p->right != nullptr) ? p->right->height : 0;
+    p->height = std::max(l, r) + 1;
+
+    p->balance = r - l;
+    rebalance(p);
+    return p;
+}
--- 09_avl_decart_tree/test_avltree.cpp
+++ 09_avl_decart_tree/test_avltree.cpp
@@ -8,7 +8,7 @@ TEST(AvlNode, Init)
     std::cout << "v = " << n.value << " h = " << n.height << " b = " << n.balance
               << " l = " << n.left << " r = " << n.right << "\n";
     EXPECT_EQ(-1, n.value);
-    EXPECT_EQ(-1, n.height);
+    EXPECT_EQ(1, n.height);
     EXPECT_EQ(0, n.balance);
     EXPECT_EQ(nullptr, n.left);
     EXPECT_EQ(nullptr, n.right);
@@ -35,12 +35,95 @@ TEST(AvlTree, Init)
 /*
  * ['J', 'F', 'P', 'D', 'G', 'L', 'V', 'C', 'N', 'S', 'X', 'Q', 'U']
  * [ 74,  70,  80,  68,  71,  76,  86,  67,  78,  83,  88,  81,  85]
+ *
+ * ['C', 'D', 'F', 'G', 'J', 'L', 'N', 'P', 'Q', 'S', 'U', 'V', 'X']
+ * [ 67,  68,  70,  71,  74,  76,  78,  80,  81,  83,  85,  86,  88]
  */
 TEST(AvlTree, Insert)
 {
     hw::AvlTree t;
-    t.insert(74);
+    t.insert(67);
     EXPECT_EQ(1, t.height());
     EXPECT_EQ(0, t.balance());
     EXPECT_FALSE(t.empty());
+
+    t.insert(68);
+    EXPECT_EQ(2, t.height());
+    EXPECT_EQ(1, t.balance());
+    auto l = t.root()->left;
+    auto r = t.root()->right;
+    EXPECT_EQ(1, r->height);
+    EXPECT_EQ(0, r->balance);
+    EXPECT_EQ(nullptr, l);
+
+    t.insert(70);
+    EXPECT_EQ(3, t.height());
+    EXPECT_EQ(2, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(2, r->height);
+    EXPECT_EQ(1, r->balance);
+    EXPECT_EQ(nullptr, l);
+
+    /*
+    t.insert(70);
+    EXPECT_EQ(2, t.height());
+    EXPECT_EQ(0, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(1, l->height);
+    EXPECT_EQ(0, l->balance);
+    EXPECT_EQ(1, r->height);
+    EXPECT_EQ(0, r->balance);
+
+    t.insert(71);
+    EXPECT_EQ(3, t.height());
+    EXPECT_EQ(-1, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(2, l->height);
+    EXPECT_EQ(-1, l->balance);
+    EXPECT_EQ(1, r->height);
+    EXPECT_EQ(0, r->balance);
+
+    t.insert(74);
+    EXPECT_EQ(3, t.height());
+    EXPECT_EQ(-1, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(2, l->height);
+    EXPECT_EQ(0, l->balance);
+    EXPECT_EQ(1, r->height);
+    EXPECT_EQ(0, r->balance);
+
+    t.insert(76);
+    EXPECT_EQ(3, t.height());
+    EXPECT_EQ(0, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(2, l->height);
+    EXPECT_EQ(0, l->balance);
+    EXPECT_EQ(2, r->height);
+    EXPECT_EQ(-1, r->balance);
+
+    t.insert(78);
+    EXPECT_EQ(3, t.height());
+    EXPECT_EQ(0, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(2, l->height);
+    EXPECT_EQ(0, l->balance);
+    EXPECT_EQ(2, r->height);
+    EXPECT_EQ(0, r->balance);
+
+    t.insert(80);
+    EXPECT_EQ(4, t.height());
+    EXPECT_EQ(-1, t.balance());
+    l = t.root()->left;
+    r = t.root()->right;
+    EXPECT_EQ(3, l->height);
+    EXPECT_EQ(-1, l->balance);
+    EXPECT_EQ(2, r->height);
+    EXPECT_EQ(0, r->balance);
+*/
 }
