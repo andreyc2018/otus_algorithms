bfe159d0c4086c3c0c759790d71e6c94035d356e
Anery Chichinov <sysdos286@yahoo.com>
2019-08-14 23:47:33 -0400

    added catch2 instead of gtest

--- 24_encryption_algorithms/test_xorcrypt.cpp
+++ 24_encryption_algorithms/test_xorcrypt.cpp
@@ -1,5 +1,43 @@
 #include "xorcrypt.h"
 #include "key.h"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Init", "XorCryptKey")
+{
+    Key k("");
+    REQUIRE(k.next() == '\0');
+
+    std::string expect = "hello";
+
+    k = expect;
+    for (int i = 0; i < 10; ++i) {
+        int idx = i % expect.size();
+        REQUIRE(expect[idx] == k.next());
+    }
+}
+
+TEST_CASE("Encrypt", "Decrypt")
+{
+    char a = 'h';
+    char k = 'a';
+    byte c = a ^ k;
+    printf("0x%X ^ 0x%X = 0x%X\n", a, k, c);
+    printf("0x%X ^ 0x%X = 0x%X\n", c, k, c ^ k);
+    REQUIRE('h' == (c ^ k));
+}
+
+TEST_CASE("Xor", "EncryptMsgDecrypt")
+{
+    std::string key = "strong key";
+    std::string msg = "hidden message";
+
+    auto encrypted = encrypt_xor(msg, key);
+    auto decrypted = decrypt_xor(encrypted, key);
+
+    REQUIRE(msg == decrypted);
+}
+#if 0
 #include <gtest/gtest.h>
 
 TEST(XorCryptKey, Init)
@@ -36,3 +74,5 @@ TEST(Xor, EncryptMsgDecrypt)
 
     EXPECT_EQ(msg, decrypted);
 }
+#endif
+
